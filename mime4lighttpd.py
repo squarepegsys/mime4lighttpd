b0c89a3a296b45edaa91ad067d4b387d6fc39e32
Mike Hostetler <mike@squarepegsystems.com>
2010-12-23 21:46:02 -0600

    code cleanup

--- mime4lighttpd.py
+++ mime4lighttpd.py
@@ -1,10 +1,13 @@
 #!/usr/local/bin/python
+import sys
 
-fp = file("mime.types")
+fp = file(sys.argv[1])
 out = file("mime.out","w")
 
+out.write("mimetype.assign = (\n")
+
 for line in fp:
-   if line.find(";")<0 or line.strip()==None:
+   if line.find(";")<0 or not line.strip():
       continue
   
    type,extlist = line.strip()[:-1].split(None,1)
@@ -12,6 +15,8 @@ for line in fp:
    for ext in extlist.split():
       outstr="\".%s\" => \"%s\",\n" %(ext,type)
       out.write(outstr)
+
+out.write(")\n")
 
 fp.close()
 out.close()
